c440363ae068c104c2553930b8c04cf67b2c5beb
PaPluckfii <sumeet1996das@gmail.com>
2021-04-13 11:11:25 +0530

    Initial commit

---
+++ app/src/main/java/com/buildweek/bbc/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.buildweek.bbc.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.buildweek.bbc.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
